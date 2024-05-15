4794844903b962a15a27b58e1b70dc50c64d3995
jonas costa <costajonas520@gmail.com>
2024-05-14 21:11:53 -0300

    so arrumei o passado

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function pegarNome() {
-let nome = document.querySelector("#nome").value
+let nome = document.querySelector("#nome").value.toLowerCase();
 
-if (nome === "Jonas") {
+if (nome === "jonas") {
 console.log("USUÁRIO JÁ CADASTRADO");
 }else{
 console.log("USUÁRIO NOVO")
