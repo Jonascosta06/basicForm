f775559aaeb12f593c223b927ba01d9c8600386c
jonas costa <costajonas520@gmail.com>
2024-05-14 22:45:09 -0300

    acrescentado a função de verificar se o compo nome está vaxio

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function pegarNome() {
 let nome = document.querySelector("#nome").value.toLowerCase();
 
-if (nome === "jonas") {
+if(nome == ""){
+    alert('Preencha o campo nome')
+    }
+
+else if (nome === "jonas") {
 console.log("USUÁRIO JÁ CADASTRADO");
 }else{
 console.log("USUÁRIO NOVO")
